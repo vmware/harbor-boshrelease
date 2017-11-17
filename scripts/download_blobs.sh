d1c281e5902313c9c789a14bdd0ccf869acaf40b
Steven Zou <szou@vmware.com>
2017-11-17 11:01:18 +0800

    rename the harbor installer artifact to unique fixed name

--- scripts/download_blobs.sh
+++ scripts/download_blobs.sh
@@ -8,12 +8,12 @@ cd $files_dir
 wget https://download.docker.com/linux/static/stable/x86_64/docker-17.06.2-ce.tgz
 wget -O docker-compose-Linux-x86_64-1.16.1 https://github.com/docker/compose/releases/download/1.16.1/docker-compose-Linux-x86_64
 wget https://www.openssl.org/source/openssl-1.0.2l.tar.gz
-wget https://github.com/vmware/harbor/releases/download/v1.2.0/harbor-offline-installer-v1.2.0.tgz
+wget -O harbor-offline-installer-latest.tgz https://github.com/vmware/harbor/releases/download/v1.2.0/harbor-offline-installer-v1.2.0.tgz
 
 cd -
 cd ..
 bosh add-blob $files_dir/docker-17.06.2-ce.tgz docker/docker-17.06.2-ce.tgz
 bosh add-blob $files_dir/docker-compose-Linux-x86_64-1.16.1 docker/docker-compose-Linux-x86_64-1.16.1
 bosh add-blob $files_dir/openssl-1.0.2l.tar.gz library/openssl-1.0.2l.tar.gz
-bosh add-blob $files_dir/harbor-offline-installer-v1.2.0.tgz harbor/harbor-offline-installer-v1.2.0.tgz
+bosh add-blob $files_dir/harbor-offline-installer-latest.tgz harbor/harbor-offline-installer-latest.tgz
 
