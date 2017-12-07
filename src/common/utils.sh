1d29f2875baccd1dce880b0a92d3fb0e28fe94b7
Jesse Hu <huh@vmware.com>
2017-12-07 17:17:08 +0800

    Load harbor docker images in harbor job prestart instead of job start
    This can solve the canary watch timeout issue #3735, because the time
    of prestart execution is not included in the canary_watch_time.
    This patch moves docker related utils and bosh env vars to
    src/common/utils.sh, and the key code for loading images is
    in jobs/harbor/templates/bin/pre-start.erb.
    
    Issue: https://github.com/vmware/harbor/issues/3735

---
+++ src/common/utils.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+RUN_DIR=/var/vcap/sys/run
+LOG_DIR=/var/vcap/sys/log
+JOB_DIR=/var/vcap/jobs
+PACKAGE_DIR=/var/vcap/packages
+
+DOCKER_RUN_DIR=$RUN_DIR/docker
+DOCKER_PACKAGE_DIR=${PACKAGE_DIR}/docker
+DOCKER_DAEMON_SOCK=${DOCKER_RUN_DIR}/dockerd.sock
+DOCKER_HOST="unix://$DOCKER_DAEMON_SOCK"
+DOCKER_CMD="${DOCKER_PACKAGE_DIR}/bin/docker -H $DOCKER_HOST"
+
+log() {
+  echo [`date`] $*
+}
+
+waitForDockerd() {
+  sleep_time=3
+  timeout=60
+  count=0
+  while ! $DOCKER_CMD version 2>&1
+  do
+    log "Docker daemon is not running. Waiting for $sleep_time seconds then check again."
+    sleep $sleep_time
+    count=$((count + sleep_time));
+    if [ $count -ge $timeout ]; then
+      log "Error: Docker daemon is still not running after $timeout seconds."
+      exit 1
+    fi
+  done
+  log "Docker daemon is running"
+}
+
+waitForDockerJobPrestart() {
+  # All prestart scripts run in parallel.
+  # The docker job prestart (jobs/docker/templates/bin/pre-start.erb
+  # should be able to complete in 5 seconds.
+  sleep 5
+
+  sleep_time=2
+  timeout=60
+  count=0
+  while ! grep -q "^docker:" /etc/group
+  do
+    sleep $sleep_time
+    count=$((count + sleep_time));
+    if [ $count -ge $timeout ]; then
+      log "Error: docker job prestart doesn't exit in $timeout seconds."
+      exit 1
+    fi
+  done
+  log "docker job prestart completed"
+}
