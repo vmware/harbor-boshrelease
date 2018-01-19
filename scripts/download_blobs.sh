4a90d5b1b317d5b30f720590bb1f35e21b2cf88e
Steven Zou <szou@vmware.com>
2018-01-19 15:31:55 +0800

    Update harbor offline installer to version v1.3.0-rc1-267-gdb8511b

--- scripts/download_blobs.sh
+++ scripts/download_blobs.sh
@@ -8,7 +8,7 @@ cd $files_dir
 wget https://download.docker.com/linux/static/stable/x86_64/docker-17.06.2-ce.tgz
 wget -O docker-compose-Linux-x86_64-1.16.1 https://github.com/docker/compose/releases/download/1.16.1/docker-compose-Linux-x86_64
 wget https://www.openssl.org/source/openssl-1.0.2l.tar.gz
-wget -O harbor-offline-installer-latest.tgz https://storage.googleapis.com/harbor-builds/harbor-offline-installer-v1.3.0-rc1-93-g051d16d.tgz
+wget -O harbor-offline-installer-latest.tgz https://storage.googleapis.com/harbor-builds/harbor-offline-installer-v1.3.0-rc1-267-gdb8511b.tgz
 wget https://dl.google.com/go/go1.9.2.linux-amd64.tar.gz
 
 cd -
