8e3e64f7199631159187b88d7c8e5fcb377b40b9
stonezdj <stonezdj@gmail.com>
2018-09-10 10:53:37 +0800

    Change package items

--- scripts/add_blobs.sh
+++ scripts/add_blobs.sh
@@ -7,15 +7,17 @@ mkdir -p $files_dir
 cd $files_dir
 if [ "$1" != "--skip-download" ]; then
   wget https://download.docker.com/linux/static/stable/x86_64/docker-18.06.0-ce.tgz
-  wget -O docker-compose-Linux-x86_64-1.16.1 https://github.com/docker/compose/releases/download/1.16.1/docker-compose-Linux-x86_64
+  wget -O docker-compose-Linux-x86_64 https://github.com/docker/compose/releases/download/1.22.0/docker-compose-Linux-x86_64
   #wget -O harbor-offline-installer-latest.tgz https://storage.googleapis.com/harbor-builds/harbor-offline-installer-latest.tgz
   wget -O harbor-offline-installer-latest.tgz https://storage.googleapis.com/harbor-releases/release-1.6.0/harbor-offline-installer-v1.6.0-build.340.tgz
-  wget https://dl.google.com/go/go1.9.2.linux-amd64.tar.gz
+  wget https://dl.google.com/go/go1.9.7.linux-amd64.tar.gz
+  wget https://www.python.org/ftp/python/2.7.15/Python-2.7.15.tgz
 fi
 cd -
 cd ..
 bosh add-blob $files_dir/docker-18.06.0-ce.tgz docker/docker-18.06.0-ce.tgz
-bosh add-blob $files_dir/docker-compose-Linux-x86_64-1.16.1 docker/docker-compose-Linux-x86_64-1.16.1
+bosh add-blob $files_dir/docker-compose-Linux-x86_64 docker/docker-compose-Linux-x86_64
 bosh add-blob $files_dir/harbor-offline-installer-latest.tgz harbor/harbor-offline-installer-latest.tgz
-bosh add-blob $files_dir/go1.9.2.linux-amd64.tar.gz go/go1.9.2.linux-amd64.tar.gz
+bosh add-blob $files_dir/go1.9.7.linux-amd64.tar.gz go/go1.9.7.linux-amd64.tar.gz
+bosh add-blob $files_dir/Python-2.7.15.tgz python/Python-2.7.15.tgz
 
