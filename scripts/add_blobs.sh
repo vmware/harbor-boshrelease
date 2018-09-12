2dcff74407aaf8cb508c37ae9a5aab65df17afd1
stonezdj <stonezdj@gmail.com>
2018-09-12 17:13:58 +0800

    remove golang pkg

--- scripts/add_blobs.sh
+++ scripts/add_blobs.sh
@@ -10,7 +10,6 @@ if [ "$1" != "--skip-download" ]; then
   wget -O docker-compose-Linux-x86_64 https://github.com/docker/compose/releases/download/1.22.0/docker-compose-Linux-x86_64
   #wget -O harbor-offline-installer-latest.tgz https://storage.googleapis.com/harbor-builds/harbor-offline-installer-latest.tgz
   wget -O harbor-offline-installer-latest.tgz https://storage.googleapis.com/harbor-releases/release-1.6.0/harbor-offline-installer-v1.6.0-build.340.tgz
-  wget https://dl.google.com/go/go1.9.7.linux-amd64.tar.gz
   wget https://www.python.org/ftp/python/2.7.15/Python-2.7.15.tgz
   wget http://mirrors.kernel.org/ubuntu/pool/main/n/nfs-utils/nfs-common_1.2.8-9ubuntu12_amd64.deb
   wget http://security.ubuntu.com/ubuntu/pool/main/libe/libevent/libevent-2.0-5_2.0.21-stable-2ubuntu0.16.04.1_amd64.deb
@@ -24,7 +23,6 @@ cd ..
 bosh add-blob $files_dir/docker-18.06.0-ce.tgz docker/docker-18.06.0-ce.tgz
 bosh add-blob $files_dir/docker-compose-Linux-x86_64 docker/docker-compose-Linux-x86_64
 bosh add-blob $files_dir/harbor-offline-installer-latest.tgz harbor/harbor-offline-installer-latest.tgz
-bosh add-blob $files_dir/go1.9.7.linux-amd64.tar.gz go/go1.9.7.linux-amd64.tar.gz
 bosh add-blob $files_dir/Python-2.7.15.tgz python/Python-2.7.15.tgz
 bosh add-blob $files_dir/nfs-common_1.2.8-9ubuntu12_amd64.deb nfs-common/nfs-common_1.2.8-9ubuntu12_amd64.deb
 bosh add-blob $files_dir/libevent-2.0-5_2.0.21-stable-2ubuntu0.16.04.1_amd64.deb nfs-common/libevent-2.0-5_2.0.21-stable-2ubuntu0.16.04.1_amd64.deb
